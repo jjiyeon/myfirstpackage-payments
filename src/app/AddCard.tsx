bd88a635374b4dadbd9cfec3a9bc09133f493bb9
jiyeon <ejy1105@gmail.com>
2024-03-30 22:19:16 +0900

    fix: 생각해보니 카드 선택 모달에서 카드사를 선택하면 수정할 수가 없다. 수정할 수 있게 카드 컴포넌트에도 이벤트 걸어주기

--- src/app/AddCard.tsx
+++ src/app/AddCard.tsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from 'react'
 import { CardInfoContext, UpdateCardInfoContext } from '../context/paymentContext'
-import { CardBox, CardCvc, CardName, CardNumber, CardPassword, CardType, CardDate } from '../components'
+import { CardBox, CardCvc, CardName, CardNumber, CardPassword, CardDate, CardType } from '@/components'
 import { StepProps } from './Payments'
 import { CardListContext } from '../context/cardListContext'
 import ui from '../styles/index.module.css'
@@ -8,10 +8,16 @@ import ui from '../styles/index.module.css'
 const AddCard = ({ onStep }: StepProps) => {
   const cardInfo = useContext(CardInfoContext)
   const updateCardInfo = useContext(UpdateCardInfoContext)
-
   const cardList = useContext(CardListContext)
 
   const [isShowModal, setIsShowModal] = useState(false)
+
+  const handleNextButtonClick = () => {
+    if (!cardInfo.cardType?.name) return setIsShowModal((state) => !state)
+    if (Object.keys(cardInfo).length < 9) return alert('모든 정보를 입력해주세요.')
+    updateCardInfo({ ...cardInfo, cardNo: cardList.length || 0 })
+    if (onStep) onStep({ step: 'complete' })
+  }
 
   return (
     <main>
@@ -22,23 +28,14 @@ const AddCard = ({ onStep }: StepProps) => {
             <button onClick={() => onStep && onStep({ step: 'list' })}>&lt;</button>
             &nbsp; 카드 추가
           </h2>
-          <CardBox />
+          <CardBox isOpenCardType={setIsShowModal} />
           <CardNumber />
           <CardDate />
           <CardName />
           <CardCvc />
           <CardPassword />
           <div className={ui['button-box']}>
-            <button
-              onClick={() => {
-                if (!cardInfo.cardType) return setIsShowModal((state) => !state)
-
-                updateCardInfo({ ...cardInfo, cardNo: cardList.length || 0 })
-                if (onStep) onStep({ step: 'complete' })
-              }}
-            >
-              다음
-            </button>
+            <button onClick={handleNextButtonClick}>다음</button>
           </div>
         </div>
       </div>
