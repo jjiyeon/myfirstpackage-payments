88f678dea1436ce565a3c8798c7add3b38804231
jiyeon <ejy1105@gmail.com>
2024-04-01 00:37:38 +0900

    feat: input props 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,8 +1,10 @@
+import { ForwardedRef, MutableRefObject, RefObject, forwardRef } from 'react'
 import './input.css'
 
 interface InputProps {
   type: string
   value?: string
+  name?: string
   primary?: boolean
   backgroundColor?: string
   size?: 'xsmall' | 'small' | 'medium' | 'large'
@@ -12,13 +14,25 @@ interface InputProps {
   disabled?: boolean
   label?: string
   className?: string
+  refObj?: MutableRefObject<string[]>
 }
 
-export const Input = ({ type, primary, size = 'large', backgroundColor, ...props }: InputProps) => {
-  return (
-    <div className={'input-container'}>
-      <span className={'input-title'}>{props.label}</span>
-      <input type={type} className={[`${props.className ?? 'input-basic'}`, `input-${size}`].join(' ')} style={{ backgroundColor }} {...props} />
-    </div>
-  )
-}
+export const Input = forwardRef(
+  ({ type, backgroundColor, size, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
+    return (
+      <div className={'input-container'}>
+        <span className={'input-title'}>{props.label}</span>
+        <input
+          ref={ref}
+          type={type}
+          className={[`${props.className ?? 'input-basic'}`, `input-${size}`].join(' ')}
+          style={{ backgroundColor }}
+          {...props}
+          autoFocus
+        />
+      </div>
+    )
+  }
+)
+
+Input.displayName = 'Input'
