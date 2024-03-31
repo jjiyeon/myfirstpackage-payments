aba55ea106d98f09a0e6d610c265e425f498d76a
jiyeon <ejy1105@gmail.com>
2024-04-01 00:38:08 +0900

    feat: cardinfo 컬러값과 숫자 매칭 타입 추가

--- src/context/paymentContext.tsx
+++ src/context/paymentContext.tsx
@@ -1,10 +1,30 @@
 import { createContext, useState } from 'react'
 
 export type CardCompanyName = '국민' | '현대' | '삼성' | '우리' | '아멕스' | '비자' | '비씨' | '롯데'
+export type CardStartNum = '55' | '34' | '37' | '36' | '53' | '40' | '48' | '44'
+export const cardStartNumArray = ['55', '34', '37', '36', '53', '40', '48', '44']
+export type CardColor = 'tomato' | 'gray' | 'yellow' | 'blue' | 'green' | 'pupul' | 'orange' | 'black' | 'default'
+
+export const cardStartMatching: CardMatch = {
+  55: { type: '국민', color: 'tomato' },
+  34: { type: '현대', color: 'gray' },
+  37: { type: '삼성', color: 'yellow' },
+  36: { type: '우리', color: 'blue' },
+  53: { type: '아멕스', color: 'green' },
+  40: { type: '비자', color: 'pupul' },
+  48: { type: '비씨', color: 'orange' },
+  44: { type: '롯데', color: 'black' },
+}
+interface CardMatch {
+  [name: number]: {
+    type: string
+    color: CardColor
+  }
+}
 export type CardCompany = {
-  name: CardCompanyName
-  theme: 'tomato' | 'gray' | 'yellow' | 'blue' | 'green' | 'pupul' | 'orange' | 'black'
-  startNum: '55' | '34' | '37' | '36' | '53' | '40' | '48' | '44'
+  name?: CardCompanyName | null
+  theme?: CardColor | null
+  startNum?: CardStartNum | null
 }
 export type CardNumber = {
   first?: string
@@ -14,13 +34,13 @@ export type CardNumber = {
 }
 export type CardInfo = {
   cardNo?: number
-  cardNumber: CardNumber | null
-  month: string | null
-  year: string | null
-  name: string | null
-  password: string | null
-  cvc: string | null
-  cardType: CardCompany | null
+  cardNumber?: CardNumber | null
+  month?: string | null
+  year?: string | null
+  name?: string | null
+  password?: string | null
+  cvc?: string | null
+  cardType?: CardCompany | null
   cardAlias?: string
 }
 
