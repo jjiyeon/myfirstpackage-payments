3e03d6f0b3efaf16661cdfd167595da3f89e0780
jiyeon <ejy1105@gmail.com>
2024-04-01 00:38:51 +0900

    feat: 툴팁 적용

--- src/components/CardInfo/CardCvc/index.tsx
+++ src/components/CardInfo/CardCvc/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { CardInfoContext, UpdateCardInfoContext } from '../../../context/paymentContext'
 import { Input } from '../../common/Input'
 import ui from '@/styles/index.module.css'
@@ -7,9 +7,19 @@ export const CardCvc = () => {
   const cardInfo = useContext(CardInfoContext)
   const updateCardInfo = useContext(UpdateCardInfoContext)
 
+  const [onFocus, setOnFocus] = useState(false)
+
+  //css 처리도 방법일듯..
+  const handleMouseEnter = () => {
+    setOnFocus(true)
+  }
+  const handleMouseLeave = () => {
+    setOnFocus(false)
+  }
+
   if (!cardInfo) return null
   return (
-    <div className={ui['row-container']}>
+    <div className={`${ui['row-container']} ${ui['tooltip-align']}`}>
       <Input
         type="password"
         value={cardInfo.cvc || ''}
@@ -20,6 +30,12 @@ export const CardCvc = () => {
         size="small"
         label="보안코드(CVC/CVV)"
       />
+      <div className={`${ui['tooltip-wrapper']}`} onMouseOver={handleMouseEnter} onMouseOut={handleMouseLeave}>
+        <span>?</span>
+      </div>
+      <p className={`${ui['tooltip']} ${ui[onFocus ? 'hover' : '']}`} style={{ fontSize: '12px' }}>
+        카드 뒷면의 3자리를 입력해주세요.
+      </p>
     </div>
   )
 }
